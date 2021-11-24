0bc0b6e7f5bff8c86283feeabb018b1667d13e0a
César Córdoba <cordobacesar29@gmail.com>
2021-11-24 20:38:41 -0300

    balanceOf & ownerOf function

--- contracts/ZombieOwnership.sol
+++ contracts/ZombieOwnership.sol
@@ -5,4 +5,12 @@ import "./erc721.sol";
 
 contract ZombieOwnership is ZombieBattle, ERC721 {
 
+  function balanceOf(address _owner) public view returns (uint256 _balance) {
+    return ownerZombieCount[_owner];
+  }
+
+  function ownerOf(uint256 _tokenId) public view returns (address _owner) {
+    return zombieToOwner[_tokenId];
+  }
+
 }
